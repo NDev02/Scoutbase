7f133efb4c51848bba274e878e160a982f2c1fc8
ndev02 <ngordondeveloper@gmail.com>
2019-02-09 01:16:14 -0600

    First commit

---
+++ javascript/core.js
@@ -0,0 +1,73 @@
+
+if (navigator.serviceWorker) {
+    if (navigator.serviceWorker.controller) {
+        console.log('[PWA Builder] active service worker found, no need to register')
+    } else {
+        navigator.serviceWorker.register('pwabuilder-sw.js', {
+            scope: './'
+        }).then(function (reg) {
+            console.log('Service worker has been registered for scope:' + reg.scope);
+        });
+    }
+}
+
+window.addEventListener('load', e => {
+    toggleConfigurer();
+});
+
+
+function toggleConfigurer() {
+    let toggles = document.querySelectorAll('.toggle');
+
+    function icon(hasTitle) {
+        let i = document.createElement('i');
+        i.className = "material-icons vis-toggle";
+        i.innerHTML = 'visibility';
+        i.setAttribute('show', true)
+        i.onclick = function () {
+            let nowShow = this.getAttribute('show');
+            if (nowShow == 'true') {
+                i.innerHTML = 'visibility_off';
+                this.setAttribute('show', false);
+                let children;
+                if (hasTitle)
+                    children = i.parentElement.parentElement.children;
+                else
+                    children = i.parentElement.children;
+                for (let child of children) {
+                    if (child.className.indexOf('toggle') == -1 && child.className.indexOf('keep') == -1)
+                        child.hidden = false;
+                }
+            }
+            else {
+                i.innerHTML = 'visibility';
+                this.setAttribute('show', true);
+                let children;
+                if (hasTitle)
+                    children = i.parentElement.parentElement.children;
+                else
+                    children = i.parentElement.children;
+                for (let child of children) {
+                    if (child.className.indexOf('toggle') == -1 && child.className.indexOf('keep') == -1)
+                        child.hidden = true;
+                }
+            }
+        }
+        return i;
+    }
+
+    for (let toggle of toggles) {
+        let placedChanger = false;
+        let children = toggle.children;
+        for (let child of children) {
+            if (child.className.indexOf('keep') != -1) {
+                child.appendChild(icon(true));
+                placedChanger = true;
+            }
+            if (child.className.indexOf('toggle') == -1 && child.className.indexOf('keep') == -1)
+                child.hidden = true;
+        }
+        if (!placedChanger)
+            toggle.appendChild(icon());
+    }
+}
