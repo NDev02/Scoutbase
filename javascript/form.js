b1e98bb2765a0de8dae07e924af9f3019c49342d
ndev02 <ngordondeveloper@gmail.com>
2019-02-13 16:56:07 -0500

    form functions

--- javascript/form.js
+++ javascript/form.js
@@ -1,4 +1,7 @@
 let inputs = {
+    'team': 0,
+    'match': 0,
+    'alliance': 'red',
     'rocket-top-cargo': 0,
     'rocket-top-hatch': 0,
     'rocket-mid-cargo': 0,
@@ -6,7 +9,10 @@ let inputs = {
     'rocket-low-cargo': 0,
     'rocket-low-hatch': 0,
     'cargo-hatch': 0,
-    'cargo-cargo': 0
+    'cargo-cargo': 0,
+    'hab-level': 'low',
+    'how-climb': 'self',
+    'comment': ''
 }
 
 function inc(type) {
@@ -19,4 +25,32 @@ function dec(type) {
         return;
     inputs[type] --;
     document.querySelector(`#${type}`).innerHTML = inputs[type];
+}
+
+function setTeam() {
+    inputs['team'] = event.target.value;
+}
+
+function setMatch() {
+    inputs['match'] = event.target.value;
+}
+
+function setAlliance() {
+    inputs['alliance'] = event.target.value;
+}
+
+function setClimbTo() {
+    inputs['level'] = event.target.value;
+}
+
+function setClimb() {
+    inputs['how-climb'] = event.target.value;
+}
+
+function setComment() {
+    inputs['comment'] = event.target.value;
+}
+
+function submit() {
+    
 }
