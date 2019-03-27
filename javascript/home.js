bf922d88ea051c671d183c1e69484d22451574b0
Nathan Gordon <ngordondeveloper@gmail.com>
2019-03-26 21:21:21 -0500

    Update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function() {
-    let event = location.search.split("=")[1];
-    localStorage.setItem('event', event);
+    if(location.search != "") {
+        let event = location.search.split("=")[1];
+        localStorage.setItem('event', event);
+    }
     if(localStorage.getItem('name') == null) {
         alert("You need to setup your profile first");
         location.href = "./settings.html";
