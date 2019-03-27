f3a186e823257a4cecc943f64af5da538a65f2c8
Nathan Gordon <ngordondeveloper@gmail.com>
2019-03-26 21:13:22 -0500

    Update form.js

--- javascript/form.js
+++ javascript/form.js
@@ -79,25 +79,5 @@ function submit() {
     inputs.event = inputs.event.toLowerCase();
     basecamp.postMatchData(inputs.event, inputs, res => {
         alert(res.msg);
-        document.querySelector("#form").reset();
-        let inputs = {
-            'event': localStorage.getItem('event'),
-            'team': 0,
-            'match': 0,
-            'alliance': 'red',
-            'rocket-top-cargo': 0,
-            'rocket-top-hatch': 0,
-            'rocket-mid-cargo': 0,
-            'rocket-mid-hatch': 0,
-            'rocket-low-cargo': 0,
-            'rocket-low-hatch': 0,
-            'cargo-hatch': 0,
-            'cargo-cargo': 0,
-            'level': 'no-climb',
-            'how-climb': 'self',
-            'comment': '',
-            'scouter': '',
-            'time-stamp': ''
-        };
-    });
+        location.reload();
 }
